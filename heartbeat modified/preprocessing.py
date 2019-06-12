ae65d5bc57c87a0aaf82159d777eff4a4d2bb23c
Bruna Beletti <bruna_beletti@hotmail.com>
2019-06-12 17:11:26 -0300

    mudei vaaaaarias coisas

--- heartbeat modified/preprocessing.py
+++ heartbeat modified/preprocessing.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("set_b.csv")
 df = df.drop("sublabel", axis=1)
-df = df.replace(["extrastole","murmur"],"abnormal")
+df = df.replace(["extrastole", "murmur"], "abnormal")
 df.to_csv("pre-set_b.csv")
 
-y, sr = lb.load("./set_b/extrastole__127_1306764300147_C2.wav",duration=4)
-dur = lb.get_duration(y)
+y, sr = lb.load("./set_b/extrastole__127_1306764300147_C2.wav")
+
+plt.figure(figsize=(10, 5))
+lbd.waveplot(y,sr=sr,x_axis='time')
+plt.title("Waveplot")
+plt.show()
 
 X = lb.stft(y)
 Xdb = lb.amplitude_to_db(abs(X))
-plt.figure(figsize=(14,5))
-lbd.specshow(Xdb,sr=sr,x_axis='time',y_axis='hz')
+plt.figure(figsize=(12, 5))
+lbd.specshow(Xdb, sr=sr ,x_axis='time', y_axis='hz')
+plt.ylim(0,2000)
+plt.title("Spectrogram")
 plt.colorbar()
-plt.show()
-
-print("duration: ", dur)
-
-plt.figure(figsize=(16,3))
-lbd.waveplot(y,sr=sr)
 plt.show()
