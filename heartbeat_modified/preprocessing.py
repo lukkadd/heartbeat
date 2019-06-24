5a928d841ee77e6f41d02d8c0548bd2f627cbd94
Bruna Beletti <bruna_beletti@hotmail.com>
2019-06-24 16:03:03 -0300

    batata

--- heartbeat_modified/preprocessing.py
+++ heartbeat_modified/preprocessing.py
@@ -1,7 +1,4 @@
 import pandas as pd
-import librosa as lb
-import librosa.display as lbd
-import matplotlib.pyplot as plt
 from sklearn import preprocessing   
 
 df = pd.read_csv("set_b.csv")
@@ -12,21 +9,4 @@ for index, row in df.iterrows():
 le = preprocessing.LabelEncoder()
 df['label'] = le.fit_transform(df['label'])
 
-
 df.to_csv("pre-set_b.csv")
-
-# y, sr = lb.load("./set_b/extrastole__127_1306764300147_C2.wav")
-
-# plt.figure(figsize=(10, 5))
-# lbd.waveplot(y,sr=sr,x_axis='time')
-# plt.title("Waveplot")
-# plt.show()
-
-# X = lb.stft(y)
-# Xdb = lb.amplitude_to_db(abs(X))
-# plt.figure(figsize=(12, 5))
-# lbd.specshow(Xdb, sr=sr ,x_axis='time', y_axis='hz')
-# plt.ylim(0,2000)
-# plt.title("Spectrogram")
-# plt.colorbar()
-# plt.show()
