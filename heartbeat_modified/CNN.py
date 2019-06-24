af27a34a25fe28fe3d9975d8069c98174bd1b3d8
lukkadd <lukkadd@yahoo.com.br>
2019-06-24 19:46:53 -0300

    saporra2

--- heartbeat_modified/CNN.py
+++ heartbeat_modified/CNN.py
@@ -5,20 +5,29 @@ from keras.utils import to_categorical
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
+import numpy as np
 SPEC_DIR = './spectograms/'
 df = pd.read_csv('pre-set_b.csv')
-data = []
+X = []
+Y = []
 for index, row in df.iterrows():
-    data.append( (plt.imread(SPEC_DIR+ row[2].replace('.wav','.jpg')), row[3]) )
+    X.append(plt.imread(SPEC_DIR+ row[2].replace('.wav','.jpg')))
+    Y.append(row[3])
 
-data = pd.DataFrame(data,index=None)
+x_train, x_test, y_train, y_test = train_test_split(X,Y,test_size=0.3,random_state=42)
 
-(x_train,y_train),(x_test,y_test) = train_test_split(data,test_size=0.3,random_state=42,stratify=data['label'])
+#reshaping images
+length = len(x_train)
+x_train = np.array(x_train)
+x_train = x_train.reshape(length,400,800,3)
 
+length = len(x_test)
+x_test = np.array(x_test)
+x_test = x_test.reshape(length,400,800,3)
 
-x_train = x_train.reshape(len(x_train),400,800,3)
-x_test = x_test.reshape(len(x_test),400,800,3)
+#categorical
+y_test = to_categorical(y_test)
+y_train = to_categorical(y_train)
 
 #Building model
 model = Sequential()
@@ -33,10 +42,17 @@ model.add(Dense(2, activation='softmax'))
 
 #Compiling model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
+print('******************************************************************************')
 #Training model
 model.fit(x_train, y_train, verbose=0, epochs=3)
 
+model.summary()
+
+
+print('******************************************************************************')
 # #Testing model
 y_train_pred = model.predict(x_train)
 y_test_pred = model.predict(x_test)
+
+print(y_train_pred)
+print(y_test_pred)
