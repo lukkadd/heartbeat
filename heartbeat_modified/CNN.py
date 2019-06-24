7ae0e4e9fc181cc9008626a67708ac850fd7befb
lukkadd <lukkadd@yahoo.com.br>
2019-06-24 14:53:54 -0300

    removed noisy and replaced Btraining_ in file names

---
+++ heartbeat_modified/CNN.py
@@ -0,0 +1,22 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+import pandas as pd
+
+df = pd.read_csv('pre-set_b.csv')
+
+
+
+
+
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
